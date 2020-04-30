4d6849a117dd2a89a189c72ec3ce891e5a6a4607
Jason Botwick <jwagon@gmail.com>
2020-04-30 03:12:28 -0400

    Basic sample app with partial data

---
+++ FullStackDevExercise/Program.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Hosting;
+
+namespace FullStackDevExercise
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      BootstrapData();
+      CreateHostBuilder(args).Build().Run();
+    }
+
+    private static void BootstrapData()
+    {
+      var connectionStringBuilder = new SqliteConnectionStringBuilder();
+      connectionStringBuilder.DataSource = "./dolittle.db";
+
+      using var connection = new SqliteConnection(connectionStringBuilder.ConnectionString);
+      connection.Open();
+      SetupDB(connection);
+      CreateOwnersTable(connection);
+      CreatePetsTable(connection);
+    }
+
+    private static void SetupDB(SqliteConnection connection) { 
+      var createTable = connection.CreateCommand();
+      createTable.CommandText = @"  PRAGMA foreign_keys = ON;";
+    }
+
+    private static void CreateOwnersTable(SqliteConnection connection)
+    {
+      var createTable = connection.CreateCommand();
+      createTable.CommandText = @"
+        CREATE TABLE IF NOT EXISTS owners
+        (
+          id INTEGER PRIMARY KEY
+          , first_name VARCHAR(50) NOT NULL
+          , last_name VARCHAR(50) NOT NULL
+        )
+      ";
+      createTable.ExecuteNonQuery();
+    }
+
+    private static void CreatePetsTable(SqliteConnection connection)
+    {
+      var createTable = connection.CreateCommand();
+      createTable.CommandText = @"
+        CREATE TABLE IF NOT EXISTS pets
+        (
+          id INTEGER PRIMARY KEY
+          , owner_id INT NOT NULL
+          , type VARCHAR(50) NOT NULL
+          , name VARCHAR(50) NOT NULL
+          , age INT NOT NULL
+          , FOREIGN KEY (owner_id) REFERENCES owners(id) ON DELETE CASCADE ON UPDATE NO ACTION 
+        )
+      ";
+      createTable.ExecuteNonQuery();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# MedStudy Software Engineer Coding Exercises
+
+This project contains a set of projects that can be used for the purposes of interviewing software or other engineers for employment at MedStudy.
